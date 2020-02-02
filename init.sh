c8686ec20cb83e5621ffdd5625d07eff6ad27653
Jacob Young <youngjacob@dingtalk.com>
2020-02-02 22:03:42 +0800

    modified init.sh
    Signed-off-by: Jacob Young <youngjacob@dingtalk.com>

--- init.sh
+++ init.sh
@@ -5,4 +5,15 @@ echo "$USER ALL=(ALL:ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$USER
 
 sudo apt install -y vim git openssh-server
 
+cat ~/.vimrc << EOF
+set nu
+set ts=4
+set hlsearch
+set java_space_errors=1
+EOF
+
+git config --global  user.email "youngjacob@dingtalk.com"
+git config --global  user.name "Jacob Young"
+
+echo 3 | sudo update-alternatives --config editor 
 
