4e5301fd3e60f89c3310fdec5bbe17acdd445768
Jacob Young <youngjacob@dingtalk.com>
2020-02-02 22:24:23 +0800

    modified init.sh
    Signed-off-by: Jacob Young <youngjacob@dingtalk.com>

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 echo -e "yangao\nyangao" | sudo passwd
 echo "$USER ALL=(ALL:ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$USER
 
-sudo apt install -y vim git openssh-server
+sudo apt install -y curl vim git openssh-server
 
 cat ~/.vimrc << EOF
 set nu
@@ -17,3 +17,5 @@ git config --global  user.name "Jacob Young"
 
 echo 3 | sudo update-alternatives --config editor 
 
+curl --output code.deb https://vscode.cdn.azure.cn/stable/26076a4de974ead31f97692a0d32f90d735645c0/code_1.41.1-1576681836_amd64.deb
+sudo dpkg -i code.deb
